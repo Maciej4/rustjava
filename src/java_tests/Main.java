029ace89842c3766194db474782870f88db860c5
Maciej4 <m@maciej.me>
2022-08-31 01:57:27 -0700

    better java example

---
+++ src/java_tests/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = add(x, 10);
+        System.out.println(y);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/java_tests/Test.java
+++
@@ -1,5 +0,0 @@
-class Test {
-    int double(int x) {
-        return x * 2;
-    }
-}
