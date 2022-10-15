6e7bbc6b5b0ae41e04855f639213656ada846719
Maciej4 <m@maciej.me>
2022-10-14 23:53:49 -0700

    add parser context struct

--- src/javac.rs
+++ src/javac.rs
@@ -8,6 +8,7 @@ trait NodeExt {
     fn child_by_kind(&self, kind: &str) -> Option<Node>;
     fn children_by_kind(&self, kind: &str) -> Vec<Node>;
     fn get_children(&self) -> Vec<Node>;
+    fn name_from_identifier(&self, source: &[u8]) -> Result<String, String>;
     fn count_node_kind_recursive(&self, kind: &str) -> usize;
     fn depth(&self) -> usize;
     fn print_tree(&self);
@@ -27,6 +28,18 @@ impl NodeExt for Node<'_> {
 
     fn get_children(&self) -> Vec<Node> {
         self.children(&mut self.walk()).collect()
+    }
+
+    fn name_from_identifier(&self, source: &[u8]) -> Result<String, String> {
+        let child = match self.child_by_kind("identifier") {
+            Some(child) => child,
+            None => return Err(format!("Could not find identifier for {}", self.kind())),
+        };
+
+        match child.utf8_text(source) {
+            Ok(text) => Ok(text.to_string()),
+            Err(err) => Err(format!("Failed to parse name of {}: {}", child.kind(), err)),
+        }
     }
 
     fn count_node_kind_recursive(&self, kind: &str) -> usize {
@@ -72,8 +85,8 @@ impl NodeExt for Node<'_> {
 struct SuperLocals {
     pub local_names: Vec<String>,
     pub local_types: Vec<PrimitiveType>,
-    // TODO: add support for variables that are not primitives
-    pub reference_classes: HashMap<usize, u16>, // index of local, class name
+    // TODO: add support for arrays
+    pub reference_classes: HashMap<usize, usize>, // index of local, class name
 }
 
 impl SuperLocals {
@@ -84,7 +97,7 @@ impl SuperLocals {
     }
 
     pub fn get_local_type(&self, index: &usize) -> Result<PrimitiveType, String> {
-        match self.local_types.get(index.clone()) {
+        match self.local_types.get(*index) {
             Some(local_type) => Ok(local_type.clone()),
             None => Err(format!("Local variable with index {} not found", index)),
         }
@@ -98,11 +111,40 @@ impl SuperLocals {
 
 #[derive(Debug)]
 struct MethodInfo {
-    pub method_name: String,
     pub class_name: String,
+    pub method_name: String,
     pub signature: String,
     pub variables: SuperLocals,
     pub return_type: PrimitiveType,
+}
+
+#[derive(Debug)]
+struct FieldInfo {
+    pub class_name: String,
+    pub field_name: String,
+    pub signature: String,
+    pub field_type: PrimitiveType,
+}
+
+#[derive(Debug)]
+struct ParserContext {
+    pub classes: Vec<String>,
+    pub methods: Vec<MethodInfo>,
+    pub fields: Vec<FieldInfo>,
+}
+
+impl ParserContext {
+    pub fn find_method(&self, class_name: String, method_name: String) -> Option<&MethodInfo> {
+        self.methods
+            .iter()
+            .find(|method| method.class_name == class_name && method.method_name == method_name)
+    }
+
+    pub fn find_field(&self, class_name: String, field_name: String) -> Option<&FieldInfo> {
+        self.fields
+            .iter()
+            .find(|field| field.class_name == class_name && field.field_name == field_name)
+    }
 }
 
 fn type_node_to_primitive_type(node: Node) -> Result<PrimitiveType, String> {
@@ -112,6 +154,7 @@ fn type_node_to_primitive_type(node: Node) -> Result<PrimitiveType, String> {
         // [ = array
         "boolean_type" => Ok(PrimitiveType::Boolean),
         "array_type" => Ok(PrimitiveType::Reference),
+        "type_identifier" => Ok(PrimitiveType::Reference),
         "void_type" => Ok(PrimitiveType::Null),
         "integral_type" | "floating_point_type" => {
             let node_deep =
@@ -148,35 +191,18 @@ fn parse_method_info(
     class_name: String,
     source: &[u8],
 ) -> Result<MethodInfo, String> {
-    let method_name = match method_node.child_by_kind("identifier") {
-        Some(node) => match node.utf8_text(source) {
-            Ok(text) => text.to_string(),
-            Err(err) => return Err(format!("Failed to parse method identifier: {}", err)),
-        },
-        None => return Err(String::from("Method is missing identifier")),
-    };
+    let method_name = method_node.name_from_identifier(source)?;
 
     let formal_params = match method_node.child_by_kind("formal_parameters") {
         Some(formal_params_node) => formal_params_node,
-        None => return Err(String::from("Formal parameters not found")),
+        None => return Err(String::from("Method is missing formal_parameters node")),
     };
 
     let mut param_names = vec![];
     let mut param_types = vec![];
 
     for param in formal_params.children_by_kind("formal_parameter") {
-        let param_name = match param.child_by_kind("identifier") {
-            Some(node) => match node.utf8_text(source) {
-                Ok(text) => text.to_string(),
-                Err(err) => {
-                    return Err(format!(
-                        "Failed to parse formal parameter identifier: {}",
-                        err
-                    ))
-                }
-            },
-            None => return Err(String::from("Formal parameter is missing identifier")),
-        };
+        let param_name = param.name_from_identifier(source)?;
 
         let param_type = match param.child(0) {
             Some(node) => type_node_to_primitive_type(node)?,
@@ -189,7 +215,7 @@ fn parse_method_info(
 
     let method_return_type = match method_node.child(1) {
         Some(method_return_type_node) => type_node_to_primitive_type(method_return_type_node)?,
-        None => return Err(String::from("Method return type not found")),
+        None => return Err(String::from("Method missing return type")),
     };
 
     let mut signature = format!(
@@ -230,13 +256,7 @@ fn generate_method_list(class_node: &Node, source: &[u8]) -> Result<Vec<MethodIn
         None => return Err(String::from("Class body node has no parent")),
     };
 
-    let class_name = match class_declaration_node.child_by_kind("identifier") {
-        Some(node) => match node.utf8_text(source) {
-            Ok(text) => text.to_string(),
-            Err(err) => return Err(format!("Failed to parse class identifier: {}", err)),
-        },
-        None => return Err(String::from("Class is missing identifier")),
-    };
+    let class_name = class_declaration_node.name_from_identifier(source)?;
 
     for method_node in class_node.children_by_kind("method_declaration") {
         methods.push(parse_method_info(&method_node, class_name.clone(), source)?);
@@ -250,8 +270,9 @@ fn generate_method_list(class_node: &Node, source: &[u8]) -> Result<Vec<MethodIn
 fn parse_expression(
     node: &Node,
     source: &[u8],
+    parser_context: &ParserContext,
     super_locals: &SuperLocals,
-    constant_pool: &[ConstantPoolEntry],
+    constant_pool: &mut Vec<ConstantPoolEntry>,
 ) -> Result<(Vec<Instruction>, PrimitiveType), String> {
     let mut instructions = vec![];
     let mut expression_type = PrimitiveType::Null;
@@ -268,6 +289,7 @@ fn parse_expression(
                     return Err(format!("Failed to parse decimal integer literal: {}", err))
                 }
             };
+
             instructions.push(Instruction::Const(Primitive::Int(value)));
         }
         "identifier" => {
@@ -304,12 +326,13 @@ fn parse_expression(
             };
 
             let (left_instructions, left_type) =
-                parse_expression(&left, source, super_locals, constant_pool)?;
+                parse_expression(&left, source, parser_context, super_locals, constant_pool)?;
 
             let (right_instructions, right_type) =
-                parse_expression(&right, source, super_locals, constant_pool)?;
+                parse_expression(&right, source, parser_context, super_locals, constant_pool)?;
 
-            if left_type.matches(&right_type) {
+            if !left_type.matches(&right_type) {
+                // TODO: implement automatic type widening
                 return Err(format!(
                     "Binary expression has mismatched types: {:?} and {:?}",
                     left_type, right_type
@@ -320,14 +343,14 @@ fn parse_expression(
             instructions.extend(right_instructions);
             expression_type = left_type;
 
-            match operator.as_str() {
-                "+" => instructions.push(Instruction::Add(expression_type.clone())),
-                "-" => instructions.push(Instruction::Sub(expression_type.clone())),
-                "*" => instructions.push(Instruction::Mul(expression_type.clone())),
-                "/" => instructions.push(Instruction::Div(expression_type.clone())),
-                "%" => instructions.push(Instruction::Rem(expression_type.clone())),
+            instructions.push(match operator.as_str() {
+                "+" => Instruction::Add(expression_type.clone()),
+                "-" => Instruction::Sub(expression_type.clone()),
+                "*" => Instruction::Mul(expression_type.clone()),
+                "/" => Instruction::Div(expression_type.clone()),
+                "%" => Instruction::Rem(expression_type.clone()),
                 _ => return Err(format!("Unknown binary operator {}", operator)),
-            }
+            })
         }
         "parenthesized_expression" => {
             let expression = match node.child(1) {
@@ -339,7 +362,13 @@ fn parse_expression(
                 }
             };
 
-            return parse_expression(&expression, source, super_locals, constant_pool);
+            return parse_expression(
+                &expression,
+                source,
+                parser_context,
+                super_locals,
+                constant_pool,
+            );
         }
         "method_invocation" => {
             todo!()
@@ -357,7 +386,7 @@ fn parse_expression(
                 }
             };
 
-            let class_index = match constant_pool.to_vec().find_class(&class_name) {
+            let class_index = match constant_pool.find_class(&class_name) {
                 Some(index) => index,
                 None => return Err(format!("Class {} not found in constant pool", class_name)),
             };
@@ -365,10 +394,82 @@ fn parse_expression(
             instructions.push(Instruction::New(class_index as usize));
             instructions.push(Instruction::Dup);
 
-            // TODO: finish this
+            let arguments = match node.child_by_kind("argument_list") {
+                Some(node) => node,
+                None => {
+                    return Err(String::from(
+                        "Object creation expression is missing arguments",
+                    ))
+                }
+            };
+
+            let mut argument_types = vec![];
+
+            for i in 0..arguments.child_count() {
+                let argument = match arguments.child(i) {
+                    Some(node) => node,
+                    None => return Err(format!("Failed to parse argument {}", i)),
+                };
+
+                let (argument_instructions, argument_type) = parse_expression(
+                    &argument,
+                    source,
+                    parser_context,
+                    super_locals,
+                    constant_pool,
+                )?;
+
+                instructions.extend(argument_instructions);
+                argument_types.push(argument_type);
+            }
+
+            let arguments_count =
+                arguments.child_count() - arguments.children_by_kind(",").len() - 2;
+
+            let method_type = format!(
+                "({})V",
+                argument_types
+                    .iter()
+                    .map(|a| a.as_letter())
+                    .collect::<String>()
+            );
+
+            let method_index = 0; // TODO: find method index
+                                  // TODO: set expression_type to the return type of the method
+
+            instructions.push(Instruction::InvokeSpecial(method_index));
         }
         "field_access" => {
-            todo!()
+            let class_or_object_name = match node.child(0) {
+                Some(node) => match node.utf8_text(source) {
+                    Ok(text) => text.to_string(),
+                    Err(err) => {
+                        return Err(format!("Failed to parse class or object name: {}", err))
+                    }
+                },
+                None => return Err(String::from("Field access is missing class or object name")),
+            };
+
+            let field_name = match node.child(2) {
+                Some(node) => match node.utf8_text(source) {
+                    Ok(text) => text.to_string(),
+                    Err(err) => return Err(format!("Failed to parse field name: {}", err)),
+                },
+                None => return Err(String::from("Field access is missing field name")),
+            };
+
+            if let Some(index) = super_locals.find_local(&class_or_object_name) {
+                let field_index = 0; // TODO: find field index
+                                     // TODO: set expression_type to the type of the field
+
+                instructions.push(Instruction::Load(index, PrimitiveType::Reference));
+                instructions.push(Instruction::GetField(field_index));
+            } else {
+                let field_index = 0; // TODO: find field index
+                                     // TODO: set expression_type to the type of the field
+
+                instructions.push(Instruction::GetStatic(field_index));
+            }
         }
         _ => return Err(format!("Unknown expression type {}", node.kind())),
     }
@@ -391,8 +492,16 @@ pub fn parse_to_class(code: String) -> Result<Vec<Class>, String> {
 
     let class = root_node.child_by_kind("class_declaration").unwrap();
     let class_body = class.child_by_kind("class_body").unwrap();
+    let class_name = class.name_from_identifier(source)?;
 
     println!("Methods: {:?}", generate_method_list(&class_body, source));
+
+    // TODO: generate method list for every class in project
+    let parser_context = ParserContext {
+        classes: vec![class_name],
+        methods: generate_method_list(&class_body, source)?,
+        fields: vec![],
+    };
 
     Err(String::from("Not implemented"))
 }
